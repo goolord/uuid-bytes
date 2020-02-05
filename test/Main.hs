9ab9bb8f2d14b3011a0a8a7a9237fda029064cb1
Zachary Churchill <zacharyachurchill@gmail.com>
2020-02-05 14:39:22 -0500

    unhyphenated tests

--- test/Main.hs
+++ test/Main.hs
@@ -20,10 +20,19 @@ unitTests = testGroup "Unit tests"
       decodeHyphenated uuidBytes @?= Just uuidW128
   , testCase "UUID encoding/decoding idempotence test" $
       decodeHyphenated (fromByteArray $ encodeHyphenated uuidW128) @?= Just uuidW128
+  , testCase "UUIDUH encoding test" $
+      fromByteArray (encodeUnhyphenated uuidW128) @?= uuidBytesUH
+  , testCase "UUIDUH decoding test" $
+      decodeUnhyphenated uuidBytesUH @?= Just uuidW128
+  , testCase "UUIDUH encoding/decoding idempotence test" $
+      decodeUnhyphenated (fromByteArray $ encodeUnhyphenated uuidW128) @?= Just uuidW128
   ]
 
 uuidBytes :: Bytes
 uuidBytes = fromAsciiString "123e4567-e89b-12d3-a456-426655440000"
+
+uuidBytesUH :: Bytes
+uuidBytesUH = fromAsciiString "123e4567e89b12d3a456426655440000"
 
 uuidW128 :: Word128
 uuidW128 = 24249434048109030647017182302883282944
